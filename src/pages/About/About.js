089e39882515f7414c65baed7ac1c5283bc978f8
Rorber Rodríguez Arcaya <dalilarf@nauta.cu>
2022-10-23 16:05:29 -0400

    Antes de hacer data table

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,15 +1,14 @@
 import { Component } from "react";
-//import axios from "axios";
+import axios from "axios";
 
-//const url = "http://localhost:8000/sistema-reg/byunit/45";
+const url = "http://localhost:8000/sistema-reg/byunit/45";
 
 export class About extends Component {
   componentDidMount() {
-    /* axios.get(url).then((response) => {
+    axios.get(url).then((response) => {
       // handle success
       console.log(response.data);
-    });*/
-    console.log("Prueba");
+    });    
   }
   render() {
     return(
